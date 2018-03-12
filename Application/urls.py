5676a43c8c2855e7b167f8088f97ed4a29db84fe
TRINADH KOYA <trinadhkoya9@gmail.com>
2018-03-12 22:35:52 +0530

    homepage

--- Application/urls.py
+++ Application/urls.py
@@ -4,5 +4,10 @@ from Application import views
 
 urlpatterns = [
     url(r'', views.index, name='index'),
-    url(r'^about.html/$',views.about,name='about')
+    url(r'^about/$', views.about, name='about'),
+    url(r'^gallery/$', views.gallery, name='gallery'),
+    url(r'^resume/$', views.resume, name='resume'),
+    url(r'^blog/$', views.blog, name='blog'),
+    url(r'^contact/$', views.contact, name='contact'),
+
 ]
