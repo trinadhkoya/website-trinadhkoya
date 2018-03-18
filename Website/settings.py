0b3af7e4009eb016a1331add51b22c5777a9a831
TRINADH KOYA <trinadhkoya9@gmail.com>
2018-03-18 11:41:20 +0530

    STATIC ROOT

--- Website/settings.py
+++ Website/settings.py
@@ -119,12 +119,4 @@ USE_L10N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
-
-# Additional locations of static files
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-# TEMPLATE_DIRS = (
-#     os.path.join(SETTINGS_PATH, "templates"),
-# )
+STATIC_ROOT = os.path.join(BASE_DIR, 'static').replace('\\', '/')
